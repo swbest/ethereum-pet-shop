e1a65f35aa101806046bb3131716732e556b7dd4
Wong Jin Fu Shaun <sw_best@hotmail.com>
2020-02-07 17:52:58 +0800

    Update Adoption.sol

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -4,7 +4,7 @@ contract Adoption {
 
     address[16] public adopters;
 
-    // Adopting a pe
+    // Adopting a pet
     function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);
 
